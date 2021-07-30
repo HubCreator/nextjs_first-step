2ecc206ef67f0b4e9baf4a945bdbebe29ef30e81
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-07-30 12:09:04 +0900

    Use router.push on navbar li elements

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { MdFingerprint } from "react-icons/md";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
@@ -8,6 +9,7 @@ import { Button } from "./Button";
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const router = useRouter();
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -27,50 +29,36 @@ const Navbar = () => {
       <IconContext.Provider value={{ color: "#fff" }}>
         <div className="navbar">
           <div className="navbar-container container">
-            {/* <Link to="/" className="navbar-logo" onClick={closeMobileMenu}> */}
             <div className="navbar-logo" onClick={closeMobileMenu}>
               <MdFingerprint className="navbar-icon" />
               LAVISH
             </div>
 
-            {/* </Link> */}
             <div className="menu-icon" onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </div>
             <ul className={click ? "nav-menu active" : "nav-menu"}>
               {NavbarData.map((item, index) => {
                 return (
-                  <li key={index}>
-                    {/* <Link to={item.path} className={item.cName}> */}
+                  <li key={index} onClick={() => router.push(`${item.path}`)}>
                     <div className={item.cName}>{item.title}</div>
-                    {/* </Link> */}
                   </li>
                 );
               })}
 
               <li className="nav-btn">
                 {button ? (
-                  //   <Link to="/sign-up" className="btn-link">
                   <div className="btn-link">
                     <Button buttonStyle="btn--outline" buttonSize="btn--medium">
                       SIGN UP1
                     </Button>
                   </div>
                 ) : (
-                  //   </Link>
-                  //   <Link
-                  //     to="/sign-up"
-                  //     className="btn-link"
-                  //     onClick={closeMobileMenu}
-                  //   >
-
                   <div className="btn-link" onClick={closeMobileMenu}>
                     <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
                       SIGN UP2
                     </Button>
                   </div>
-
-                  //   </Link>
                 )}
               </li>
             </ul>

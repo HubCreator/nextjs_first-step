7005cf60c570e8c6fb08f2aad9bf845b33edc3a6
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-07-30 14:51:59 +0900

    Fixed it by giving width and height to images

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -3,14 +3,16 @@ import Link from "next/link";
 export default function ItemList({ list }) {
   return (
     <div className="item-list">
-      {/* {console.log(list[0])} */}
       {list.map((item, index) => {
         return (
           <>
             <Link href={`/view/${item.id}`}>
               <div className="item" key={index}>
                 <div className="item-name">{item.name}</div>
-                <img className="item-img" src={item.image_link} />
+                <div
+                  className="item-img"
+                  style={{ backgroundImage: `url(${item.image_link})` }}
+                />
                 <div className="item-price">{item.price} $</div>
                 <div className="item-description">{item.description}</div>
               </div>

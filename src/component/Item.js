5ac868d97bd31125099140add22534c870c67c01
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-01 18:02:50 +0900

    I'm making detail page, and auth section is done and working space section is not yet

--- src/component/Item.js
+++ src/component/Item.js
@@ -1,34 +1,38 @@
 const Item = ({ data }) => {
   return (
     <div className="single-item-page">
-      {/* <div className="single-item">
-        <div className="single-item-name">{data.name}</div>
-        <div
-          className="single-item-img"
-          style={{ backgroundImage: `url(${data.image_link})` }}
-        />
-        <div className="single-item-price">{data.price} $</div>
-        <div className="single-item-description">{data.description}</div>
-      </div> */}
       <div className="sections">
         <div className="info-section">
           <div
-            className="info-section__main-note"
+            className="info-section__main-img"
             style={{ backgroundImage: `url(${data.image_link})` }}
           />
+
           <div className="info-section__auth">
-            <div className="info-section__auth__name">Leo</div>
-            <div className="info-section__auth__createdAt">2020.7.22</div>
-          </div>
-          <div className="info-section__description-container">
-            <div className="info-section__description">
-              {" "}
-              안녕하세요. This area is made for description
+            <div className="info-section__auth__content">
+              <div className="info-section__auth__content__name">Leo</div>
+              <div className="info-section__auth__content__createdAt">
+                2020.7.22
+              </div>
             </div>
+          </div>
+
+          <div className="info-section__description">
+            안녕하세요. This area is made for description
           </div>
         </div>
 
-        <div className="workspace-section"></div>
+        <div className="workspace-section">
+          <div className="workspace-section__title"></div>
+          <div className="workspace-section__content">
+            <div className="workspace-section__number"></div>
+            <div className="workspace-section__img"></div>
+            <div className="workspace-section__play-controller">
+              <div className="play-controller__playBtn"></div>
+              <div className="play-controller__playBar"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

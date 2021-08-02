8c911d454a80745103c4379091899a954e8be570
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-02 12:22:25 +0900

    Made my index.js to index.tsx and fixed it little bit. Couldn't use env var

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import ItemList from "../src/component/ItemList";
+
+interface dataProps {
+  list: object;
+  name: string;
+}
+
+const Home = (props: dataProps) => {
+  return (
+    <div>
+      <Head>
+        <title>Leo | Home</title>
+      </Head>
+
+      {props.list && <ItemList list={props.list} />}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const apiUrl = process.env.apiUrl;
+  const apiUrl =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
+};

79658e6d3dc8e20ebaae3b487e098940a5f3458f
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-08-05 00:10:23 +0900

    Done with it. But when I add floor to make a room like screen, the background area cause a problem

--- pages/services/index.js
+++ pages/services/index.js
@@ -23,19 +23,23 @@ const servicePage = () => {
   return (
     <div className="service-container">
       <section className="service-page">
-        <div className="service-page__skew1">
-          <h2 className="layer">Corner Text</h2>
+        <div className="service-page__leftBox">
+          <div className="service-page__skew1">
+            <h2 className="layer">Music Player</h2>
+          </div>
         </div>
-        <div className="service-page__textBox">
+
+        <div className="service-page__rightBox">
           <div className="service-page__skew2">
-            <h2 className="layer">Corner Text</h2>
+            <h2 className="layer">Music Player</h2>
           </div>
         </div>
 
         <div className="service-page__floor">
-          <h2>Hello everyone</h2>
-          <img src="./images/piano.png" className="piano" alt="piano" />
-          <img src="./images/guitar.png" className="guitar" alt="guitar" />
+          <div className="service-page__floor__instruments">
+            <img src="./images/piano.png" className="piano" alt="piano" />
+            <img src="./images/guitar.png" className="guitar" alt="guitar" />
+          </div>
         </div>
       </section>
     </div>
